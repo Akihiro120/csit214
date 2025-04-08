39dc17d0d97365734338ba916ca379fdbe2c3710
Akihiro <kevin2006phan@gmail.com>
2025-04-08 17:57:19 +1000

    tweaks

--- frontend/flydreamair/src/main.tsx
+++ frontend/flydreamair/src/main.tsx
@@ -3,6 +3,6 @@ import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-	<h1>Hello world!</h1>
+	<h1>Fly Dream Air</h1>
   </StrictMode>,
 )
