c7e9d0f130e1b3626eaea4240f6d65b01b7f639a
RyanMai <hoangmai1070@gmail.com>
2025-04-18 09:48:04 +1000

    added tanstack router and navbar component

---
+++ frontend/flydreamair/src/routes/__root.tsx
@@ -0,0 +1,17 @@
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { Navbar } from "../components/Navbar";
+
+export const Route = createRootRoute({
+	component: RootComponent,
+});
+
+function RootComponent() {
+	return (
+		<>
+			<div className="h-full grid grid-rows-[auto_1fr]">
+				<Navbar />
+				<Outlet />
+			</div>
+		</>
+	);
+}
