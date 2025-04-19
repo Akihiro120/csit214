ef705f0c416d8f59401b10bfe7b55ed5dab12cbe
Akihiro120 <127700131+Akihiro120@users.noreply.github.com>
2025-04-19 02:30:08 +0000

    Deploying to production from @ Akihiro120/csit214@ec579a35d62afa1dc68d8a29be69be8f41cd4fb7 🚀

--- assets/index-Cj1gS7nG.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as o}from"./index-Z60BboaH.js";const r=function(){return t.jsxs(t.Fragment,{children:["Content in the root route goes here",t.jsx(o,{})]})};export{r as component};
--- assets/index-Dxb5kX7O.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Z60BboaH.js";const n=function(){return o.jsx("div",{className:"text-4xl",children:'This is the "/booking/search/" page!'})};export{n as component};
