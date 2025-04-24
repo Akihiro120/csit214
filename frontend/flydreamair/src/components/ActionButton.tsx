3106b5052f34054488273134308c89af0b1044f8
RyanMai <hoangmai1070@gmail.com>
2025-04-24 21:31:41 +1000

    homepage skeleton done

--- frontend/flydreamair/src/components/ActionButton.tsx
+++ frontend/flydreamair/src/components/ActionButton.tsx
@@ -1,4 +1,4 @@
-import { JSX } from "react";
+/* import { JSX } from "react";
 
 interface Props {
 	onclick: () => void;
@@ -11,6 +11,50 @@ export function ActionButton({ onclick, children }: Props): JSX.Element {
 			onClick={onclick}
 			className="text-white bg-(--accent) hover:bg-(--hover-button) shadow-md/25 p-2 rounded-md"
 		>
+			{children}
+		</button>
+	);
+}
+ */
+import { motion } from "motion/react";
+import React from "react";
+
+interface Props {
+	className?: string;
+	children: React.ReactNode;
+	hoverOverlayTheme?: "light" | "dark";
+	onClick?: () => void;
+}
+
+export function ActionButton({ className, children, hoverOverlayTheme, onClick }: Props) {
+	const [isActive, setIsActive] = React.useState(false);
+
+	return (
+		<button
+			/* set to active on click */
+			onMouseDown={() => {
+				setIsActive(true);
+			}}
+			onMouseUp={() => {
+				setIsActive(false);
+			}}
+			onClick={onClick}
+			className={`relative bg-(--accent) text-white shadow-md/25 p-2 rounded-md ${className}`}
+		>
+			{/* darken background overlay */}
+			{hoverOverlayTheme ? (
+				<motion.div
+					variants={{
+						default: { opacity: 0 },
+						hover: { opacity: 0.16 },
+						active: { opacity: 0.22 },
+					}}
+					initial="default"
+					whileHover={isActive ? "active" : "hover"}
+					animate={isActive ? "active" : "default"}
+					className={`absolute z-30 top-0 left-0 w-full h-full rounded-[inherit] ${hoverOverlayTheme === "light" ? "bg-white" : "bg-black"}`}
+				></motion.div>
+			) : null}
 			{children}
 		</button>
 	);
--- frontend/flydreamair/vite.config.ts
+++ frontend/flydreamair/vite.config.ts
@@ -2,13 +2,14 @@ import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [tailwindcss(), TanStackRouterVite({ target: "react", autoCodeSplitting: true }), react()],
+	plugins: [svgr(), tailwindcss(), TanStackRouterVite({ target: "react", autoCodeSplitting: true }), react()],
 	base: "/",
-	server: { // Add this server configuration block
-        host: true // This makes Vite listen on all public IPs, including 127.0.0.1
-    }
+	server: {
+		// Add this server configuration block
+		host: true, // This makes Vite listen on all public IPs, including 127.0.0.1
+	},
 });
- 
