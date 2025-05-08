2b724846d9582977a3c383acb45ff07b27ddf1df
James <jblackley97@gmail.com>
2025-05-08 15:57:41 +1000

    added types for handling session state

--- frontend/flydreamair/src/type.ts
+++ frontend/flydreamair/src/type.ts
@@ -15,3 +15,34 @@ export interface FlightSearchResult {
     dept_time: string;
     arr_time: string;
 }
+
+export interface FlightUpgradeOptions {
+    meal: string;
+    baggage: string;
+    carry_on: string;
+    entertainment: string;
+}
+
+
+export interface Passanger {
+    name: string;
+    email: string;
+    phone?: string;
+    seat?: string;
+    options?: FlightUpgradeOptions;
+}
+
+
+
+export interface SessionData {
+    initialized: number;
+    to? : string;
+    from? : string;
+    dept_date? : string;
+    ret_date? : string;
+    flight_id? : string;
+    numPassengers? : number;
+    isReturn?: boolean;
+    passengers?: Passanger[];
+
+}
