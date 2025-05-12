f3d6b96b754a27cf76a7964a56d2c948fb06ae3d
James <jblackley97@gmail.com>
2025-05-12 17:55:25 +1000

    formatted changes

--- frontend/flydreamair/src/type.ts
+++ frontend/flydreamair/src/type.ts
@@ -23,7 +23,6 @@ export interface FlightUpgradeOptions {
     entertainment: string;
 }
 
-
 export interface Passanger {
     name: string;
     email: string;
@@ -32,17 +31,14 @@ export interface Passanger {
     options?: FlightUpgradeOptions;
 }
 
-
-
 export interface SessionData {
     initialized: number;
-    to? : string;
-    from? : string;
-    dept_date? : string;
-    ret_date? : string;
-    flight_id? : string;
-    numPassengers? : number;
+    to?: string;
+    from?: string;
+    dept_date?: string;
+    ret_date?: string;
+    flight_id?: string;
+    numPassengers?: number;
     isReturn?: boolean;
     passengers?: Passanger[];
-
 }
